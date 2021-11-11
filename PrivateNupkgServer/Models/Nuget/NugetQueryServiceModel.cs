2663bc98ec42d795c3c209c8a703df0b4813799d
Mustafa Salih ASLIM <salih@aslim.net>
2021-11-11 19:09:44 +0300

    fix: required attributes added

--- PrivateNupkgServer/Models/Nuget/NugetQueryServiceModel.cs
+++ PrivateNupkgServer/Models/Nuget/NugetQueryServiceModel.cs
@@ -2,16 +2,22 @@
 
 public class NugetQueryServiceModel
 {
+    [FromQuery]
     public string? q { get; set; }
 
+    [FromQuery]
     public int skip { get; set; }
 
+    [FromQuery]
     public int take { get; set; }
 
+    [FromQuery]
     public bool prerelease { get; set; }
 
+    [FromQuery]
     public string[]? supportedFramework { get; set; }
 
+    [FromQuery]
     public string? semVerLevel { get; set; }
 
     [XmlIgnore]
